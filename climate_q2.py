544d5fbf435be530fe0fc21810b3cea154885e43
gauruv-grover <gauruvgrover@gmail.com>
2023-09-14 10:47:47 +1000

    Completed Question 2

--- climate_q2.py
+++ climate_q2.py
@@ -1,20 +1,21 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+mapdata = pd.read_csv(r".\climate.csv")
 
-years = []
-co2 = []
-temp = []
+years = mapdata['Year']
+co2 = mapdata['CO2']
+temp = mapdata['Temperature']
 
 plt.subplot(2, 1, 1)
-plt.plot(years, co2, 'b--') 
-plt.title("Climate Data") 
-plt.ylabel("[CO2]") 
-plt.xlabel("Year (decade)") 
+plt.plot(years, co2, 'b--')
+plt.title("Climate Data")
+plt.ylabel("[CO2]")
+plt.xlabel("Year (decade)")
 
 plt.subplot(2, 1, 2)
-plt.plot(years, temp, 'r*-') 
-plt.ylabel("Temp (C)") 
-plt.xlabel("Year (decade)") 
-plt.show() 
-plt.savefig("co2_temp_2.png") 
+plt.plot(years, temp, 'r*-')
+plt.ylabel("Temp (C)")
+plt.xlabel("Year (decade)")
+plt.show()
+plt.savefig("co2_temp_2.png")
 
